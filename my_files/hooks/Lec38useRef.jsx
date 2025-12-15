8e1a7308fdd9f4d91e80f7a33b6b1dc0fe4c156f
Drago <technologywax.com>
2025-12-15 19:12:39 +0530

    tuf

---
+++ my_files/hooks/Lec38useRef.jsx
@@ -0,0 +1,14 @@
+
+
+function Lec38useRef() {
+    
+    return (
+        <div>
+            <h1>UseRef Hook</h1>
+            <input type="text" placeholder="Enter text here" />
+            <button>focus on input fiel</button>
+        </div>
+    )
+}
+
+export default Lec38useRef;
