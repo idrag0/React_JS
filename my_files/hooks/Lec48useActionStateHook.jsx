48796a3febf8e7dc858cae755c27688eea17de09
Drago <technologywax@gmail.com>
2025-12-28 21:52:42 +0530

    lec48

--- my_files/hooks/Lec48useActionStateHook.jsx
+++ my_files/hooks/Lec48useActionStateHook.jsx
@@ -1,47 +1,52 @@
 
 import { useActionState } from "react"
 
-function Lec48useActionStateHook(){
+function Lec48useActionStateHook() {
 
-    const handlesubmit = async (prevData, formData)=>{
+    const handlesubmit = async (prevData, formData) => {
         console.log("handle submit called");
         let name = formData.get("username")
         let pass = formData.get("password")
 
-        await new Promise(res =>setTimeout(res, 2000));
+        await new Promise(res => setTimeout(res, 2000));
 
-        if(name && pass){
-            return {message : "Data submitted successfully"}
-        }else{
-            return {error : "failed to submit data"}
+        if (name && pass) {
+            return { message: "Data submitted successfully", name, pass }
+        } else {
+            return { error: "failed to submit data", name, pass }
         }
-        
+
     }
 
-    const [data, action, pending]=useActionState(handlesubmit);
+    const [data, action, pending] = useActionState(handlesubmit);
     console.log(data);
 
 
-    
-    return(
+
+    return (
         <div>
             <h1>useActionState Hook</h1>
 
             <form action={action}>
-                <input type="text" placeholder="username" name="username"/>
+                <input type="text" placeholder="username" name="username" />
                 <br /><br />
-                <input type="text" placeholder="password" name="password"/>
+                <input type="text" placeholder="password" name="password" />
                 <br /><br />
                 <button type="submit" disabled={pending}> {pending ? "submitting" : "Submit"}</button>
 
-                {
-                    data?.error && <h3 style={{color : "red"}}>{data.error}</h3>
-                }
-                {
-                    data?.message && <h3 style={{color : "green"}}>{data.message}</h3>
-                }
+
 
             </form>
+
+            {
+                data?.error && <h3 style={{ color: "red" }}>{data.error}</h3>
+            }
+            {
+                data?.message && <h3 style={{ color: "green" }}>{data.message}</h3>
+            }
+
+            <h3>User Name : {data?.name}</h3>
+            <h3>Password : {data?.pass}</h3>
 
         </div>
     )
