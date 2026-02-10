3a0e82f69c4d5051915dd0ce33c98865bbfd2924
Gufran Ahmad <33379735+Gstarinfo@users.noreply.github.com>
2026-02-10 08:35:40 +0530

    update

--- react_router/src/files/Login.jsx
+++ react_router/src/files/Login.jsx
@@ -1,5 +1,7 @@
 
 import './Router2.css'
+import "tailwindcss";
+
 
 function Login() {
     return (
@@ -7,10 +9,11 @@ function Login() {
             <h1>Login Page</h1>
             <p>Please enter your credentials to log in.</p>
             
-            <input className="inputfield" type="text" placeholder="Username" /> <br /><br />
+            <input className="inputfield bg-red-500"s type="text" placeholder="Username" /> <br /><br />
             <input className="inputfield" type="password" placeholder="Password" />
             <br /><br />
-            <button id='loginbnt'>Login</button>
+            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' id='loginbnt'>Login</button>
+        
         </div>
     )
 }
